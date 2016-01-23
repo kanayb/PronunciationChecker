73319342e48783535d2b3cc2d25232263b60503f
kanayb <ayberkkanber@utexas.edu>
2016-01-22 20:01:57 -0600

    added method
    asdf

--- app/src/main/java/com/example/ayberk/pronunciationchecker/MainActivity.java
+++ app/src/main/java/com/example/ayberk/pronunciationchecker/MainActivity.java
@@ -5,11 +5,19 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.ibm.cio.dto.QueryResult;
+import com.ibm.cio.watsonsdk.SpeechDelegate;
+
+public class MainActivity extends AppCompatActivity implements SpeechDelegate  {
+
+    @Override
+    public void receivedMessage(int i, QueryResult queryResult) {
+        
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
